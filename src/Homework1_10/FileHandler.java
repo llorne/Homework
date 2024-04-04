088221fe4b315f7260ca774557a75666a664657d
llorne <108236451+llorne@users.noreply.github.com>
2024-04-04 19:28:54 +0300

    Update FileHandler.java

--- src/Homework1_10/FileHandler.java
+++ src/Homework1_10/FileHandler.java
@@ -1,8 +1,6 @@
 package Homework1_10;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 public class FileHandler {
     private String filePath;
